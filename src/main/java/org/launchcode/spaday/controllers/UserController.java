ead05786bedb62de8f032d5a89773a4de25ce3cc
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-30 17:59:57 -0500

    12.6.3 complete

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +16,15 @@ public class UserController {
     @GetMapping("add")
     public String displayAddUserForm() {
         return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (verify.equals(user.getPassword())) {
+            model.addAttribute("username", user.getUsername());
+            return "user/index";
+        } else {
+        return "user/add";
+        }
     }
 }
